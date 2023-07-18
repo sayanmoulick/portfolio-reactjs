227f9dcb204a390fcfa5c923b7c31da241e501b9
Sayan Moulick <sayanmoulick@gmail.com>
2023-07-18 23:18:54 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { NavBar } from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <p>Project setup</p>
+      <NavBar />
     </div>
   );
 }
